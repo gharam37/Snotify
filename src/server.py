96f66588ca931b17970dc37348dc2446b229215e
Daniel Ashraf Gerges Wahby <daniel.wahby@student.guc.edu.eg>
2018-11-30 15:23:04 +0200

    Added the related artists feature and done some html formatting

--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request,send_file,request
 from mail import mail
 import spotipy
+import sys
 import spotipy.oauth2 as oauth2
 
 
@@ -22,15 +23,28 @@ spotify = spotipy.Spotify(auth=token)
 def hello():
   if request.method == 'POST'  :
     artist=request.form['text']
-    results = spotify.search(q=artist, limit=20)
+    results = spotify.search(q=artist.encode("utf-8"), limit=20)
     top20 =''
+    outputFormatted='<h1>Top 20 Songs for '+artist.capitalize()+'</h1><hr />'
     for i, t in enumerate(results['tracks']['items']):
      top20+= ' '+ str(i)+ t['name']
-    mail_id=app.config.get('MAIL_ID')
-    mail_secret=app.config.get('MAIL_SECRET')
-    email= request.form['textinput']
-    mail(mail_id,mail_secret,email,top20)
-    return top20
+     outputFormatted+=str(i)+') '+t['name']+'<br />'
+    # mail_id=app.config.get('MAIL_ID')
+    # mail_secret=app.config.get('MAIL_SECRET')
+    # email= request.form['textinput']
+    # mail(mail_id,mail_secret,email,top20)
+    results = spotify.search(q='artist:' + artist, type='artist')
+    try:
+      name = results['artists']['items'][0]['name']
+      uri = results['artists']['items'][0]['uri']
+
+      results = spotify.artist_related_artists(uri)
+      outputFormatted+='<hr /><h1>Artists Related to '+artist+'</h1>'
+      for i,relatedArtist in enumerate(results['artists']):
+       outputFormatted+=str(i)+') '+relatedArtist['name']+'<br />'
+    except:
+      print("usage show_related.py [artist-name]")
+    return outputFormatted
   return render_template('View.html')
 
 
